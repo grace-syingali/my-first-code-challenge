a2274e1a5f36c030812dd86df0e02624404b49a2
grace-syingali <135760083+grace-syingali@users.noreply.github.com>
2023-06-20 12:06:26 +0300

    update index.js

---
+++ code 1/index.js
@@ -0,0 +1,21 @@
+const marks = parseFloat(prompt("Enter the student's marks:"));
+
+if (isNaN(marks) || marks < 0 || marks > 100) {
+  console.log("Invalid input! Marks should be between 0 and 100.");
+} else {
+  let grade;
+
+  if (marks > 79) {
+    grade = 'A';
+  } else if (marks >= 60) {
+    grade = 'B';
+  } else if (marks >= 50) {
+    grade = 'C';
+  } else if (marks >= 40) {
+    grade = 'D';
+  } else {
+    grade = 'E';
+  }
+
+  console.log(`The student's grade is: ${grade}`);
+}
